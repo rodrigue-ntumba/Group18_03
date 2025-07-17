1c5a7e9305c294edd603f622bf4bfeb5d0ea150d
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 14:12:08 +0200

    Delete 13_Exercice03.py

--- 13_Exercice03.py
+++
@@ -1,9 +0,0 @@
-nom_fichier = input("Nom du fichier à lire : ")
-try:
-    with open(nom_fichier, "r", encoding="utf-8") as f:
-        contenu = f.read()
-except FileNotFoundError:
-    print("Erreur : Fichier introuvable.")
-else:
-    print("Contenu du fichier :")
-    print(contenu)
