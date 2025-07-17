db7d8b3ebbb06cde33c6a7b27184e4b93937ea88
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 16:37:00 +0200

    Delete 07️.Listes et tuples directory

--- 07️.Listes et tuples/07_Exercice03.py
+++
@@ -1,9 +0,0 @@
-entree =  input("Entrez des nombres : ")
-liste=[float(x) for x in entree.split()]
-maximum = max(liste)
-minimum = min(liste)
-moyenne = sum(liste) / len(liste)
-
-print(f"Max : {maximum}")
-print(f"Min : {minimum}")
-print(f"Moyenne : {moyenne:.2f}")
