6bec28f09644a7531e283d2bbe76fd0c522069f3
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 14:02:00 +0200

    Delete 07_Exercice02.py

--- 07_Exercice02.py
+++
@@ -1,8 +0,0 @@
-liste = input("Entrez une liste de valeurs : ").split()
-uniques = []
-
-for val in liste:
-    if val not in uniques:
-        uniques.append(val)
-
-print("Valeurs uniques :", uniques)
