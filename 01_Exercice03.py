9f4b51de62911fbb5e501eeaaeb0e869fd006931
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 13:33:15 +0200

    Delete 01_Exercice03.py

--- 01_Exercice03.py
+++
@@ -1,6 +0,0 @@
-heures = int(input("Heures : "))
-minutes = int(input("Minutes : "))
-secondes = int(input("Secondes : "))
-
-total_secondes = heures * 3600 + minutes * 60 + secondes
-print(f"Durée totale en secondes : {total_secondes}")
