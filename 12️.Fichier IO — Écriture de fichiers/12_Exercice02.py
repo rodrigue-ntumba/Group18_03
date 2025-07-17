4285ecacd4b72b5a2ff35c411ef66370ee66d4c5
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 16:27:28 +0200

    Delete 12️.Fichier IO — Écriture de fichiers directory

--- 12️.Fichier IO — Écriture de fichiers/12_Exercice02.py
+++
@@ -1,8 +0,0 @@
-nombre = int(input("Nombre pour la table de multiplication : "))
-
-with open("table.txt", "w", encoding="utf-8") as f:
-    for i in range(1, 13):
-        ligne = f"{nombre} x {i} = {nombre * i}\n"
-        f.write(ligne)
-
-print("Table générée dans 'table.txt'.")
