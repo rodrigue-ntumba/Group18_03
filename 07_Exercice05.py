b0f17a06d22955d58b1f172043732ddb5be57995
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 14:02:57 +0200

    Delete 07_Exercice05.py

--- 07_Exercice05.py
+++
@@ -1,7 +0,0 @@
-liste = input("Entrez une liste de valeurs : ").split()
-inversée = []
-
-for i in range(len(liste) - 1, -1, -1):
-    inversée.append(liste[i])
-
-print("Liste inversée :", inversée)
