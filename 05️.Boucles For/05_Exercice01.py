35dfc0201daacc1eb2564e7e9cda4de916655c2a
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 16:35:59 +0200

    Delete 05️.Boucles For directory

--- 05️.Boucles For/05_Exercice01.py
+++
@@ -1,6 +0,0 @@
-nombre = int(input("Entrez un nombre  pour sa table de multiplication: "))
-
-print(f"\nTable de multiplication de {nombre} :")
-for i in range(1, 13):
-    resultat = nombre*i
-    print(f"{nombre} x {i} = {resultat}")
--- 05️.Boucles For/05_Exercice03.py
+++
@@ -1,7 +0,0 @@
-texte = input("Entrez une chaîne : ")
-inverse = ""
-
-for char in texte:
-    inverse = char + inverse
-
-print(f"Chaîne inversée : {inverse}")
