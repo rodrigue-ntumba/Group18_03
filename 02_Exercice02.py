ab58776a7dfe23f24a49703b494a253eeeeaf87f
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 13:34:57 +0200

    Delete 02_Exercice02.py

--- 02_Exercice02.py
+++
@@ -1,6 +0,0 @@
-n = int(input("Entrez un nombre entier : "))
-
-if n % 3 == 0 and n % 5 == 0:
-    print("Le nombre est divisible par 3 et 5.")
-else:
-    print("Le nombre n'est pas divisible par 3 et 5.")
