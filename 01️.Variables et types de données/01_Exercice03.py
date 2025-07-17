b60add57d17c9e5badf8f6dc2288fd02a9e16e96
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 16:27:54 +0200

    Delete 01️.Variables et types de données directory

--- 01️.Variables et types de données/01_Exercice03.py
+++
@@ -1,6 +0,0 @@
-heures = int(input("Nombres d'heures : "))
-minutes = int(input("Nombres de minutes : "))
-secondes = int(input("nombres de secondes : "))
-
-total_secondes = heures * 3600 + minutes * 60 + secondes
-print(f"Durée totale  : {total_secondes} secondes")
