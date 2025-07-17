e0191b442bc3b547cdcda4f85d94458d07fee27c
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 14:02:14 +0200

    Delete 07_Exercice03.py

--- 07_Exercice03.py
+++
@@ -1,9 +0,0 @@
-nombres = list(map(float, input("Entrez une liste de nombres : ").split()))
-
-maximum = max(nombres)
-minimum = min(nombres)
-moyenne = sum(nombres) / len(nombres)
-
-print(f"Maximum : {maximum}")
-print(f"Minimum : {minimum}")
-print(f"Moyenne : {moyenne:.2f}")
