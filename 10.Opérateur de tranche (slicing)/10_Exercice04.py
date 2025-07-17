058f0c6b947df99ffcd4ba9dad25581d7addaefe
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 16:37:44 +0200

    Delete 10.Opérateur de tranche (slicing) directory

--- 10.Opérateur de tranche (slicing)/10_Exercice04.py
+++
@@ -1,6 +0,0 @@
-numero = input("Entrez un numéro  : ")
-if len(numero)>3:
-    masque = "*" * (len(numero) - 3) + numero[-3:]
-    print(f"Numéro masqué :{masque}" )
-else:
-    print("Numéro trop court pour masquer ")
--- 10.Opérateur de tranche (slicing)/10_Exercice05.py
+++
@@ -1,4 +0,0 @@
-entree = input("Entrez des nombres séparés par des espaces : ")
-liste = entree.split()
-elements_pairs = liste[::2]
-print(f"Éléments aux indices pairs : {elements_pairs}" )
