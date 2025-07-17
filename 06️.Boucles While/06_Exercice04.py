0f8a7443c19556df4c01fcb7a7258d9ae1cd79b8
rodrigue-ntumba <ntumbarodrigue4@gmail.com>
2025-07-17 16:36:47 +0200

    Delete 06️.Boucles While directory

--- 06️.Boucles While/06_Exercice04.py
+++
@@ -1,8 +0,0 @@
-mdp = ""
-
-while mdp != "Python2025":
-    mdp = input("Entrez le mot de passe : ")
-    if mdp != "Python2025":
-        print("Mot de passe incorrect, réessayez")
-
-print("Mot de passe correct ! Accès autorisé.")
